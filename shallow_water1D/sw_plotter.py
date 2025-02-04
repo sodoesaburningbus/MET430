Corrected a sign error in model code.

shallow_water1D/sw_plotter.py
@@ -5,14 +5,21 @@
 import matplotlib.pyplot as pp
 import numpy as np
 
+##### START OPTIONS ######
+
 # Location of the output file
 fpath = './sw_output.csv'
 
 # Location to save outputs
 sdir = 'images/'
 
+# Interval between images
+interval = 50
+
 # Number of grid points in the model
 nx = 500
+
+#####  END OPTIONS  #####
 
 # Lists to hold the data
 time = []
@@ -40,7 +47,7 @@ x = np.array(x, dtype='float')
 # Make the plots
 fs = 14
 fw = 'bold'
-for i in range(0, u.shape[0])[::50]:
+for i in range(0, u.shape[0])[::interval]:
 
     fig, (ax1, ax2) = pp.subplots(nrows=2, constrained_layout=True)
     ax1.set_title(f'Time {time[i]:.2f} s', loc='left', ha='left', fontsize=fs, fontweight=fw)